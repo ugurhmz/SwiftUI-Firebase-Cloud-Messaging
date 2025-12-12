14838b3b9b171e365a46ccfbe33857c54a7e1270
Uğur hamzaoğlu <ugur.hmz52@gmail.com>
2025-12-12 18:22:29 +0300

    Initial Commit

---
+++ Firebase-Notification/Firebase_NotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_NotificationApp.swift
+//  Firebase-Notification
+//
+//  Created by rico on 12.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_NotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
